ec95799bfe2e6bd6e458efc2f8ceb585ae5a643f
victorernesto <88003634+victorernesto@users.noreply.github.com>
2021-08-17 09:19:32 -0600

    Cambio

---
+++ persona/persona/Entidades/Clspersona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace persona.Entidades
+{
+    class Clspersona
+    {
+    }
+}
