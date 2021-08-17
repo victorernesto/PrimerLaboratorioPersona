750de69a82e6c8d2396d21557c18bb00f661d494
victorernesto <88003634+victorernesto@users.noreply.github.com>
2021-08-17 10:09:48 -0600

    cambio2

--- persona/persona/Negocio/NClspersona.cs
+++ persona/persona/Negocio/NClspersona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using persona.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,39 @@ namespace persona.Negocio
 {
     class NClspersona
     {
+        public String observar(Clspersona persona)
+        {
+            persona.Nombre = "Martin";
+            persona.Observar = "observar";
+            return " la persona " + persona.Nombre + " esta " + persona.Observar;
+        }
+
+        public String reir(Clspersona persona)
+        {
+            persona.Reir = "si";
+            persona.Edad = 30;
+            persona.Nombre = "Martin";
+            return "la persona" + persona.Nombre + "de edad" + persona.Edad + "se esta riendo:" + persona.Reir;
+
+        }
+        
+        public String caminar(Clspersona persona)
+        {
+            persona.Caminar = "si";
+            persona.Nombre = "Martin";
+
+            return "la persona" + persona.Nombre + "puede caminar:" + persona.Caminar;
+
+        }
+
+        public String Ablar(Clspersona persona)
+        {
+            persona.Nombre = "Martin";
+
+            return "la persona " + persona.Nombre + "esta ablando";
+
+        }
+
+
     }
 }
