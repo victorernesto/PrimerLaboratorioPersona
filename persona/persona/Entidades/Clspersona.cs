7cd90c4f9c2e4fcbe226aedebf4a0901edca44f7
victorernesto <88003634+victorernesto@users.noreply.github.com>
2021-08-17 09:29:25 -0600

    cambio1

--- persona/persona/Entidades/Clspersona.cs
+++ persona/persona/Entidades/Clspersona.cs
@@ -8,5 +8,22 @@ namespace persona.Entidades
 {
     class Clspersona
     {
+        private String nombre;
+        private String apellidos;
+        private int edad;
+        private String sexo;
+        private String ablar;
+        private String caminar;
+        private String reir;
+        private String observar;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellidos { get => apellidos; set => apellidos = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+        public string Ablar { get => ablar; set => ablar = value; }
+        public string Caminar { get => caminar; set => caminar = value; }
+        public string Reir { get => reir; set => reir = value; }
+        public string Observar { get => observar; set => observar = value; }
     }
 }
